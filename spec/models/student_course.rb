c6cdd250fce42c2c8bcf886633f89672ae0cedbe
Jori Peterson <joripeterson@gmail.com>
2019-07-25 14:22:09 -0600

    Write model tests

---
+++ spec/models/student_course.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'StudentCourse' do
+	describe 'Validations' do
+		it { should validate_presence_of :grade }
+	end
+	
+	describe 'Relationships' do
+		it { should belong_to :student }
+		it { should belong_to(:courses) }
+	end
+end
